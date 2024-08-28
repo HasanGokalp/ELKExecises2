bfe0e77a55da74f7f5b12be0d4693d981934e3a8
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2024-08-28 09:45:45 +0300

    Proje dosyası ekle.

---
+++ ELKBasicApi/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Elastic.Clients.Elasticsearch.IndexManagement;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELKBasicApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("OKEY");
+        }
+    }
+}
---
+++ ELKBasicApi/Models/Log.cs
@@ -0,0 +1,8 @@
+﻿namespace ELKBasicApi.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+    }
+}
